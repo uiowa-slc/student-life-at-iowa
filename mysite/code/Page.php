96a482c6dc9e2f22524163e6fa96c619d8b4e0e0
Dustin Quam <quamsta@gmail.com>
2014-03-10 12:00:44 -0500

    removed some uisg page types

--- mysite/code/Page.php
+++ mysite/code/Page.php
@@ -45,38 +45,12 @@ class Page_Controller extends ContentController {
 	 *
 	 * @var array
 	 */
-	private static $allowed_actions = array ( 'ContactForm'
-	);
-
 	public function init() {
 		parent::init();
 
 		// Note: you should use SS template require tags inside your templates
 		// instead of putting Requirements calls here.  However these are
 		// included so that our older themes still work
-	}
-
-
-	public function ContactForm() {
-	  return ContactForm::create("ContactForm","uisg@uiowa.edu","You've received a new contact form!")
-	            ->addFields(
-	                TextField::create("Name","What is your name?"),
-	                EmailField::create("Email", "What is your email?")
-	            )
-	            // You can add fields as strings, too.
-	            ->addField("Your message//Textarea")
-	            ->setSuccessMessage("Thanks for submitting the form!")
-	            ->setSuccessURL('thanks/')
-	            ->setOnBeforeSend(function($data, $form) {
-	                  // Do stuff here. Return false to refuse the form.
-	            })
-	            ->setEmailTemplate("ContactPageEmail")
-	            ->addOmittedField("SomeField")
-	            ->setIntroText("Someone submitted a form. Here's the data.")
-	            ->addSpamProtector(
-	                HoneyPotSpamProtector::create()
-	            )
-	            ->render();
 	}
 	
 }
