b86a809bc522d16f97b9fdbfc395a0f3e0227fcb
Brandon Ewoldt <bewoldt1@gmail.com>
2014-04-30 19:58:54 -0500

    adding flex slider
    Signed-off-by: Brandon Ewoldt <bewoldt1@gmail.com>

---
+++ mysite/code/LeadershipLegacy.php
@@ -0,0 +1,50 @@
+<?php
+class LeadershipLegacy extends Page {
+
+	private static $db = array(
+
+	);
+
+	private static $has_one = array(
+
+	);
+
+	public function getCMSFields(){
+		$fields = parent::getCMSFields();
+
+		// $fields->removeByName("Content");
+		$fields->removeByName("Metadata");
+
+
+
+
+		return $fields;
+
+	}
+
+}
+class LeadershipLegacy_Controller extends Page_Controller {
+
+	/**
+	 * An array of actions that can be accessed via a request. Each array element should be an action name, and the
+	 * permissions or conditions required to allow the user to access it.
+	 *
+	 * <code>
+	 * array (
+	 *     'action', // anyone can access this action
+	 *     'action' => true, // same as above
+	 *     'action' => 'ADMIN', // you must have ADMIN permissions to access this action
+	 *     'action' => '->checkAction' // you can only access this action if $this->checkAction() returns true
+	 * );
+	 * </code>
+	 *
+	 * @var array
+	 */
+
+	public function init() {
+		parent::init();
+		// You can include any CSS or JS required by your project here.
+		// See: http://doc.silverstripe.org/framework/en/reference/requirements
+	}
+
+}
