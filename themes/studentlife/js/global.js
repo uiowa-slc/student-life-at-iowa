5aa75ee136a11c315722c7684e6fd76b5a7e6b5f
Brandon Ewoldt <bewoldt1@gmail.com>
2014-05-23 16:41:55 -0500

    adding youtube video feed.
    Signed-off-by: Brandon Ewoldt <bewoldt1@gmail.com>

--- themes/studentlife/js/global.js
+++ themes/studentlife/js/global.js
@@ -12,7 +12,8 @@ $(window).load(function() {
 	// });
 
 
-	// Masonry
+	/* Masonry
+	================================================== */
 	var $container = $('#container');
 	// initialize
 	$container.masonry({
@@ -30,31 +31,51 @@ $(window).load(function() {
 		});
 	});
 
-	// Flexslider
+	/* Flexslider
+	================================================== */
 	$('.flexslider').flexslider({
 		slideshow: true,
 		pauseOnHover: true
 	});
 
-	// Navigation
+	/* Navigation
+	================================================== */
 	$('.nav-title a').click(function() {
 		$('.nav-main-wrapper').toggleClass('expand');
 		return false;
 	})
 
-	// Waypoints
-	//$('.navbar').waypoint('sticky');
-	// $('.upcoming-events').waypoint('sticky', {offset: 60});
-	//$('.get-involved-img').waypoint('sticky', {offset: 60});
-	// $('.latest-news').waypoint('sticky', {offset: 60});
-	// $('.student-success-img').waypoint('sticky', {offset: 60});
-	// $('.culture-entertainment-img').waypoint('sticky', {offset: 60});
-	// $('.recreation-wellness-img').waypoint('sticky', {offset: 60});
-	// $('.health-safety-img').waypoint('sticky', {offset: 60});
-	// $('.navbar').waypoint(function(direction) {
-	//   $('.navbar').toggleClass('fixed-top');
-	// });
-
-
+	/* Youtube Video Feed
+	================================================== */
+	function show_my_videos(data){
+		html = ['<div class="container"><div class="row">'];
+		$(data.feed.entry).each(function(entry){
+			url = this.link[0].href;
+			url_thumbnail = this.media$group.media$thumbnail[0].url;
+			title = this.media$group.media$title.$t;
+			html.push('<div class="col-xs-6 col-md-3"><a href="'+url+'" class="video-link">');
+			html.push('<img src="'+url_thumbnail+'" alt="'+title+'"><div class="play"></div>');
+			html.push('</a><p>'+title+'</p></div>');
+		});
+		html.push('</div></div>');
+		$(".video-list").html(html.join(''));
+	}
+	$.ajax({
+		type: "GET",
+		url: "http://gdata.youtube.com/feeds/users/imubuddy/uploads?alt=json-in-script&format=5&max-results=4",
+		cache: false,
+		dataType:'jsonp',
+		success: function(data){
+			show_my_videos(data);
+			// If you want to see in console...
+			// console.log(data);
+			// console.log(data.feed.entry);
+			// $(data.feed.entry).each(function(){
+			// 	console.log(this.link[0].href);
+			// 	console.log(this.media$group.media$thumbnail[3].url);
+			// 	console.log(this.media$group.media$description.$t);
+			// });
+	  }
+	});
 
 });
