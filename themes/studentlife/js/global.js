9e0451a67d7bf8c1719e3949b85a5a4d2c2b476e
Brandon Ewoldt <bewoldt1@gmail.com>
2014-06-04 13:33:42 -0500

    adding home.js file
    Signed-off-by: Brandon Ewoldt <bewoldt1@gmail.com>

--- themes/studentlife/js/global.js
+++ themes/studentlife/js/global.js
@@ -106,40 +106,4 @@ $(window).load(function() {
 	  ]
 	});
 
-
-	// Setup variables
-	$window = $(window);
-	$slide = $('.homeSlide');
-	$body = $('body');
-
-    //FadeIn all sections
-	$body.imagesLoaded( function() {
-		setTimeout(function() {
-
-		      // Resize sections
-		      adjustWindow();
-
-		      // Fade in sections
-			  $body.removeClass('loading').addClass('loaded');
-
-		}, 800);
-	});
-
-	function adjustWindow(){
-
-		// Init Skrollr
-		var s = skrollr.init({
-		    forceHeight: false,
-		    render: function(data) {
-
-		        //Debugging - Log the current scroll position.
-		        //console.log(data.curTop);
-		    }
-		});
-
-	    // Refresh Skrollr after resizing our sections
-	    s.refresh($('.homeSlide'));
-
-	}
-
 });
--- themes/studentlife/templates/HomePage.ss
+++ themes/studentlife/templates/HomePage.ss
@@ -55,6 +55,7 @@
 	<!-- JS -->
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 	<script src="{$ThemeDir}/js/min/production-min.js"></script>
+	<script src="{$ThemeDir}/js/home.js"></script>
 	<% include Analytics %>
 </body>
 </html>
