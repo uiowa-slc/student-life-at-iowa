0d415b19864099e115ad8f304fa1798f464582e8
Dustin Quam <quamsta@gmail.com>
2019-05-22 10:56:49 -0500

    remove h1 from tinymce dropdown

--- mysite/_config.php
+++ mysite/_config.php
@@ -3,7 +3,7 @@
 use SilverStripe\Security\PasswordValidator;
 use SilverStripe\Security\Member;
 use SilverStripe\Control\Director;
-
+use SilverStripe\Forms\HTMLEditor\TinyMCEConfig;
 // remove PasswordValidator for SilverStripe 5.0
 $validator = new PasswordValidator();
 
@@ -16,3 +16,12 @@ if(Director::isLive()) {
 }
 
 HomePage::add_extension('SlHomePageExtension');
+
+
+
+TinyMCEConfig::get('cms')
+    ->addButtonsToLine(1, 'styleselect')
+    ->setOptions([
+        'importcss_append' => true,
+         'block_formats'=>'Paragraph=p;Heading 2=h2;Heading 3=h3;Heading 4=h4;Heading 5=h5;Heading 6=h6;Address=address;Pre=pre'
+    ]);
