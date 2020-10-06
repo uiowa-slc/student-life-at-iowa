67b01bd2922047f7da856da6bc507119e55da5ff
Dustin Quam <quamsta@gmail.com>
2020-10-06 09:14:06 -0500

    updated dp

--- mysite/_config.php
+++ mysite/_config.php
@@ -1,9 +1,9 @@
 <?php
 
-use SilverStripe\Security\PasswordValidator;
-use SilverStripe\Security\Member;
 use SilverStripe\Control\Director;
-use SilverStripe\Forms\HTMLEditor\TinyMCEConfig;
+use SilverStripe\Security\Member;
+use SilverStripe\Security\PasswordValidator;
+
 // remove PasswordValidator for SilverStripe 5.0
 $validator = new PasswordValidator();
 
@@ -11,13 +11,23 @@ $validator->minLength(8);
 $validator->checkHistoricalPasswords(6);
 Member::set_password_validator($validator);
 
-if(Director::isLive()) {
+if (Director::isLive()) {
 	Director::forceSSL();
 }
 
+// URLSegmentFilter->setReplacements([
+// 	'/&amp;/u' => '-and-',
+// 	'/&/u' => '-and-',
+// 	'/\s|\+/u' => '-', // remove whitespace/plus
+// 	'/[_.]+/u' => '-', // underscores and dots to dashes
+// 	'/[^A-Za-z0-9\-]+/u' => '', // remove non-ASCII chars, only allow alphanumeric and dashes
+// 	'/[\/\?=#:]+/u' => '-', // remove forward slashes, question marks, equal signs, hashes and colons in case multibyte is allowed (and non-ASCII chars aren't removed)
+// 	'/[\-]{2,}/u' => '-', // remove duplicate dashes
+// 	'/^[\-]+/u' => '', // Remove all leading dashes
+// 	'/[\-]+$/u' => '', // Remove all trailing dashes
+// ]);
+
 HomePage::add_extension('SlHomePageExtension');
-
-
 
 // TinyMCEConfig::get('cms')
 //     ->addButtonsToLine(1, 'styleselect')
