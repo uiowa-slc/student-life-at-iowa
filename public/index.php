8ba0769770f364247a7faec843e7a551314078e1
Dustin Quam <quamsta@gmail.com>
2021-02-24 16:48:08 -0600

    added static publisher

--- public/index.php
+++ public/index.php
@@ -7,13 +7,22 @@ use SilverStripe\Core\Startup\ErrorControlChainMiddleware;
 
 // Find autoload.php
 if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
-    require __DIR__ . '/../vendor/autoload.php';
+	require __DIR__ . '/../vendor/autoload.php';
 } elseif (file_exists(__DIR__ . '/vendor/autoload.php')) {
-    require __DIR__ . '/vendor/autoload.php';
+	require __DIR__ . '/vendor/autoload.php';
 } else {
-    header('HTTP/1.1 500 Internal Server Error');
-    echo "autoload.php not found";
-    exit(1);
+	header('HTTP/1.1 500 Internal Server Error');
+	echo "autoload.php not found";
+	exit(1);
+}
+
+$requestHandler = require 'staticrequesthandler.php';
+//print_r($requestHandler);
+// successful cache hit
+if (false !== $requestHandler('cache')) {
+	die;
+} else {
+	header('X-Cache-Miss: ' . date(\Datetime::COOKIE));
 }
 
 // Build request and detect flush
